df901a880418b35e93e50dff36206c79326845a9
zhoucheng <zhoucheng133@outlook.com>
2026-01-20 19:46:35 +0800

    添加window_manager配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:resizer/main_view.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = WindowOptions(
+    size: Size(800, 600),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.hidden,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
   runApp(const MainApp());
 }
 
@@ -9,11 +26,33 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+
+    final Brightness brightness = MediaQuery.of(context).platformBrightness;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      theme: ThemeData(
+          brightness: brightness,
+          fontFamily: 'PuHui', 
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.red,
+            brightness: brightness,
+          ),
+          textTheme: brightness==Brightness.dark ? ThemeData.dark().textTheme.apply(
+            fontFamily: 'PuHui',
+            bodyColor: Colors.white,
+            displayColor: Colors.white,
+          ) : ThemeData.light().textTheme.apply(
+            fontFamily: 'PuHui',
+          ),
         ),
+      home: Scaffold(
+        body: MainView()
       ),
     );
   }
---
+++ lib/main_view.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({super.key});
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> with WindowListener {
+
+  bool max=false;
+
+  @override
+  void initState() {
+    super.initState();
+    windowManager.addListener(this);
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
+  @override
+  void onWindowMaximize() {
+    super.onWindowMaximize();
+    setState(() {
+      max=true;
+    });
+  }
+
+  @override
+  void onWindowRestore() {
+    super.onWindowRestore();
+    setState(() {
+      max=false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 30,
+          child: Row(
+            children: [
+              Expanded(child: DragToMoveArea(child: Container())),
+              if(Platform.isWindows) Row(
+                children: [
+                  WindowCaptionButton.minimize(
+                    brightness: Theme.of(context).brightness,
+                    onPressed: ()=>windowManager.minimize()
+                  ),
+                  max ? WindowCaptionButton.unmaximize(
+                    brightness: Theme.of(context).brightness,
+                    onPressed: ()=>windowManager.unmaximize()
+                  ) : WindowCaptionButton.maximize(
+                    brightness: Theme.of(context).brightness,
+                    onPressed: ()=>windowManager.maximize()
+                  ),  
+                  WindowCaptionButton.close(
+                    brightness: Theme.of(context).brightness,
+                    onPressed: ()=>windowManager.close()
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
