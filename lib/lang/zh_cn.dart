d20b2808b30b80022cc9bb6a09cc76cf14c1b3a3
zhoucheng <zhoucheng133@outlook.com>
2026-01-21 20:44:36 +0800

    添加多语言

---
+++ lib/lang/zh_cn.dart
@@ -0,0 +1,3 @@
+const Map<String, String> zhCN = {
+  "add": "添加图片，你也可以拖拽图片到这里",
+};
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
+import 'package:resizer/lang/en_us.dart';
+import 'package:resizer/lang/zh_cn.dart';
+import 'package:resizer/lang/zh_tw.dart';
 import 'package:resizer/main_view.dart';
+import 'package:resizer/utils/controller.dart';
+import 'package:resizer/utils/handler.dart';
 import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await windowManager.ensureInitialized();
 
+  Get.put(Handler());
+  Get.put(Controller());
+
+  await windowManager.ensureInitialized();
   WindowOptions windowOptions = WindowOptions(
     size: Size(800, 600),
     center: true,
     backgroundColor: Colors.transparent,
     skipTaskbar: false,
     titleBarStyle: TitleBarStyle.hidden,
+    title: 'Resizer',
   );
   windowManager.waitUntilReadyToShow(windowOptions, () async {
     await windowManager.show();
     await windowManager.focus();
   });
   runApp(const MainApp());
+}
+
+class MainTranslations extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+    'en_US': enUS,
+    'zh_CN': zhCN,
+    'zh_TW': zhTW,
+  };
 }
 
 class MainApp extends StatelessWidget {
@@ -29,8 +48,11 @@ class MainApp extends StatelessWidget {
 
     final Brightness brightness = MediaQuery.of(context).platformBrightness;
 
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      translations: MainTranslations(),
+      locale: Get.deviceLocale,
+      fallbackLocale: Locale('en', 'US'),
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
