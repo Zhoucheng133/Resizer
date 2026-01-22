824fd593cda2b42bf2bc28505c642582e30dd6b0
zhoucheng <zhoucheng133@outlook.com>
2026-01-22 22:15:29 +0800

    添加图片预览

--- lib/views/config.dart
+++ lib/views/config.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:resizer/utils/controller.dart';
 
 class Config extends StatefulWidget {
   const Config({super.key});
@@ -8,8 +12,28 @@ class Config extends StatefulWidget {
 }
 
 class _ConfigState extends State<Config> {
+
+  final Controller controller=Get.find();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Obx(()=>
+              Image.file(File(controller.path.value))
+            ),
+          )
+        ),
+        Container(
+          width: 400,
+          decoration: BoxDecoration(
+            color: Colors.red
+          ),
+        )
+      ]
+    );
   }
 }
