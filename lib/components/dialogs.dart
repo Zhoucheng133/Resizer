eec2ab016a4f39444799dd7fb7bbae8d1f3238d8
zhoucheng <zhoucheng133@outlook.com>
2026-01-29 17:00:09 +0800

    添加确认对话框

--- lib/components/dialogs.dart
+++ lib/components/dialogs.dart
@@ -16,3 +16,23 @@ Future<void> showOkDialog(BuildContext context, String title, String content) as
     )
   );
 }
+
+Future<bool> showOkCancelDialog(BuildContext context, String title, String content, {String? cancelText, String? okText}) async {
+  return await showDialog(
+    context: context, 
+    builder: (BuildContext context)=>AlertDialog(
+      title: Text(title),
+      content: Text(content),
+      actions: [
+        TextButton(
+          onPressed: ()=>Navigator.of(context).pop(false), 
+          child: Text(cancelText??"cancel".tr)
+        ),
+        ElevatedButton(
+          onPressed: ()=>Navigator.of(context).pop(true), 
+          child: Text(okText??"ok".tr),
+        )
+      ]
+    )
+  );
+}
